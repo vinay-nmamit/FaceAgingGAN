1ef0b34b2b5557e69a026629241b8afe1c6d4485
varoon <varunsanjayamin75@gmail.com>
2025-01-21 11:34:49 +0530

    GAN-s2f

---
+++ Sketch-to-face-master/configs/paths_config.py
@@ -0,0 +1,20 @@
+dataset_paths = {
+	'celeba_train': 'C:/Users/akhil/OneDrive/Desktop/ASU_first_sem/EEE511-Artificial-Neural-Computation/Final_Project/Dataset/FS2K_1/photo/train',
+	'celeba_test': 'C:/Users/akhil/OneDrive/Desktop/ASU_first_sem/EEE511-Artificial-Neural-Computation/Final_Project/Dataset/FS2K_1/photo/test',
+	'celeba_train_sketch': 'C:/Users/akhil/OneDrive/Desktop/ASU_first_sem/EEE511-Artificial-Neural-Computation/Final_Project/Dataset/FS2K_1/sketch/train',
+	'celeba_test_sketch': 'C:/Users/akhil/OneDrive/Desktop/ASU_first_sem/EEE511-Artificial-Neural-Computation/Final_Project/Dataset/FS2K_1/sketch/test',
+	'celeba_train_segmentation': '',
+	'celeba_test_segmentation': '',
+	'ffhq': '',
+}
+
+model_paths = {
+	'stylegan_ffhq': '/content/stylegan2-ffhq-config-f.pt',
+	'ir_se50': '/content/model_ir_se50.pth',
+	'circular_face': 'pretrained_models/CurricularFace_Backbone.pth',
+	'mtcnn_pnet': 'pretrained_models/mtcnn/pnet.npy',
+	'mtcnn_rnet': 'pretrained_models/mtcnn/rnet.npy',
+	'mtcnn_onet': 'pretrained_models/mtcnn/onet.npy',
+	'shape_predictor': 'shape_predictor_68_face_landmarks.dat',
+	'moco': 'pretrained_models/moco_v2_800ep_pretrain.pth.tar'
+}
